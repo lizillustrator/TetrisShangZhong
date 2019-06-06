6b18a03f50c4f8152270093ce8205ccf70ddecd1
andrea_shang <andrea_shang@studnent.wayland.k12.ma.us>
2019-06-06 09:12:11 -0400

    Cell class updated

--- src/Cell.java
+++ src/Cell.java
@@ -1,13 +1,47 @@
+import java.awt.*;
+
 public class Cell {
 
-    private int row, col;
+    private Cell[][] cells;
+    private int rows, cols;
+    public static final int SIZE = 20;
     private boolean isFalling, isFallen;
 
-    public static final int SIZE = 20;
+    public Cell (int rows, int cols){
+        cells = new Cell[rows][cols];
+        rows = 20;
+        cols = 10;
+        isFalling = false;
+        isFallen = false;
 
-    public Cell(int r, int c) {
+
+        for (int r = 0; r < cells.length; r++) {
+            for (int c = 0; c < cells[0].length; c++) {
+                cells[r][c] = new Cell(r, c);
+            }
+        }
 
     }
 
 
+    public void draw(Graphics2D g2){
+
+        for (int r = 0; r < cells.length; r++) {
+            for (int c = 0; c < cells[0].length; c++) {
+                cells[r][c].draw(g2);
+            }
+        }
+
+    }
+
+
+
+
+
+
+
 }
+
+
+
+
